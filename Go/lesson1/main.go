de88cf1a9268608ba470aae193cca01b665bf390
Nkez <77111691+Nkez@users.noreply.github.com>
2021-11-24 00:02:09 +0300

    first homework

---
+++ Go/lesson1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"go/lesson1/Go/lesson1/tasks"
+)
+
+func main() {
+
+	pal := tasks.CheckPalindrome("abbba")
+	fmt.Println(pal)
+
+	matrix := [3][4]int{{0, 1, 1, 2}, {0, 5, 0, 0}, {2, 0, 0, 3}}
+	fmt.Println(tasks.MatrixElementSum(matrix))
+
+	fmt.Println(tasks.CreateWord("foo(bar(baz))blim"))
+	fmt.Println(tasks.CreateWord("foo(bar)baz"))
+	fmt.Println(tasks.CreateWord("(bar)"))
+
+}
